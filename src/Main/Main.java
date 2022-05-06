13b00340bfe78a3dac5ecf543a270bcc598d531b
Surandoran <103105070+Surandoran@users.noreply.github.com>
2022-05-06 17:54:17 +0900

    test

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,5 +1,6 @@
 package Main;
 
+<<<<<<< HEAD
 import controller.FrontController;
 import controller.SubController;
 import cosmeticdto.DTO;
@@ -9,10 +10,15 @@ import view.Viewer;
 
 import javax.swing.text.View;
 import java.sql.Connection;
+=======
+import Product.domain.ProductDAO;
+import Product.dto.ProductDTO;
+>>>>>>> 7eaa4fb (2022-05-06 (ProductDAO-ProductDTO 테스트))
 
 public class Main {
 
 	public static void main(String[] args) {
+<<<<<<< HEAD
 		//AutoController생성,AuthDAO생성,AuthDTO생성,AuthService생성
 		//시간남을시 GUIViewer 생성
 //		ProductDTO dto = new ProductDTO("수분크림", "닥터지", 12000, "50ml");
@@ -26,5 +32,13 @@ public class Main {
 //		view.product();
 
 		view.LoginMenu();
+=======
+		
+		ProductDTO dto = new ProductDTO("수분크림", "닥터지", 12000, "50ml");
+		ProductDAO dao = new ProductDAO();
+		
+		dao.Insert(dto);
+		
+>>>>>>> 7eaa4fb (2022-05-06 (ProductDAO-ProductDTO 테스트))
 	}
 }
--- src/com/cosview/domain/ProductDAO.java
+++ src/com/cosview/domain/ProductDAO.java
@@ -67,3 +67,4 @@ public class ProductDAO {
 		}
 		return false;
 	}
+
