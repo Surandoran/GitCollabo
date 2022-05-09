fa0a30dbbfc8aeaa41d4165fd33496967bb838f6
shinbo <sbg03202@naver.com>
2022-05-09 17:08:20 +0900

    ss

---
+++ src/com/cosview/view/EmployeeView.java
@@ -0,0 +1,14 @@
+package com.cosview.view;
+
+import javax.swing.JFrame;
+
+public class EmployeeView extends JFrame{
+	
+	
+	public EmployeeView() {
+		setTitle("관리시스템");
+		this.setBounds(100,100,600,400);
+	    this.setResizable(false);
+		
+	}
+}
