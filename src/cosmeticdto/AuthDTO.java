82eb81857caf42c850496fdbb2973c514e453368
Surandoran <103105070+Surandoran@users.noreply.github.com>
2022-05-09 17:10:21 +0900

    fanal

--- src/cosmeticdto/AuthDTO.java
+++ src/cosmeticdto/AuthDTO.java
@@ -3,6 +3,15 @@ package cosmeticdto;
 public class AuthDTO extends DTO{
     private String id;
     private String pw;
+    private String addr;
+    private String phone;
+
+    public AuthDTO(String id, String pw, String addr, String phone) {
+        this.id = id;
+        this.pw = pw;
+        this.addr = addr;
+        this.phone = phone;
+    }
 
     public AuthDTO(String id, String pw) {
         this.id = id;
@@ -25,11 +34,29 @@ public class AuthDTO extends DTO{
         this.pw = pw;
     }
 
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
     @Override
     public String toString() {
         return "AuthDTO{" +
                 "id='" + id + '\'' +
                 ", pw='" + pw + '\'' +
+                ", addr='" + addr + '\'' +
+                ", phone='" + phone + '\'' +
                 '}';
     }
 }
