e5cb50f644909df03c6cc3938a4b7fe54f787c26
Surandoran <103105070+Surandoran@users.noreply.github.com>
2022-05-11 12:42:54 +0900

    버튼 수정 gui수정(master)

--- src/com/cosview/main/Main.java
+++ src/com/cosview/main/Main.java
@@ -50,14 +50,14 @@ public class Main extends JFrame implements ActionListener{
       Font font1 = new Font("Serif", Font.ITALIC, 17);
       this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       
-      icon = new ImageIcon("4Cosmetic/src/image/cos.png");
+      icon = new ImageIcon("image/cos.png");
       
       p1 = new JPanel();
       p1.setBackground(Color.black);
       p1.setLayout(null);
-      txt1 = new JTextField();
+      txt1 = new JTextField("son");
       
-      txt2 = new JTextField();
+      txt2 = new JTextField("1234");
       btn1 = new JButton("Login");
       btn1.setFont(font);
       btn1.setBackground(Color.gray);
