78385c07b98b01846fbcc1a9fe84b33f80a5a58d
Surandoran <103105070+Surandoran@users.noreply.github.com>
2022-05-11 18:34:15 +0900

    gender값 넣기

--- src/com/cosview/domain/AuthDAO.java
+++ src/com/cosview/domain/AuthDAO.java
@@ -2,111 +2,127 @@ package com.cosview.domain;
 
 import com.cosview.dto.AuthDTO;
 import com.cosview.dto.MemberDTO;
+import com.cosview.view.SignUpview;
 
-public class AuthDAO extends DAO{
-	
-//	public AuthDAO() {}
-	
-	
-	public boolean MemberInsert(MemberDTO dto) {
-		try {
-			pstmt = conn.prepareStatement("insert into member_tbl values(?,?,?,?,?,?)");
-			pstmt.setString(1, dto.getId());
-			pstmt.setString(2, dto.getPw());
-			pstmt.setString(3, dto.getName());
-			pstmt.setString(4, dto.getEmail());
-			pstmt.setString(5, dto.getNickName());
-			pstmt.setString(6, dto.getAddr());
+public class AuthDAO extends DAO {
 
-			
-			int result = pstmt.executeUpdate();
-			if(result!=0) {
-				return true;
-			}
-		}catch(Exception e) {
-			e.printStackTrace();
-		}finally {
-			try {pstmt.close();}catch(Exception e) {e.printStackTrace();}
-		}
-		
-		
-		
-		return false;
-	}
-	
-	
-	//회원 로그인 1
-	public boolean MemberLogin(AuthDTO dto) {
-		try {
-			pstmt = conn.prepareStatement("select id,pw from member_tbl where id=?");
-			pstmt.setString(1,dto.getId());
-			rs = pstmt.executeQuery();
-			String tid = null;
-			String tpw = null;
-			while(rs.next()) {
-				tid = rs.getString("id");
-				tpw = rs.getString("pw");
-			}
-			
-			if(tid!=null && tpw!=null) {
-				if(tid.equals(dto.getId()) && tpw.equals(dto.getPw())) {
-					return true;
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}finally {
-			try {
-				rs.close();
-				pstmt.close();
-				
-			} catch (Exception e2) {
-				// TODO: handle exception
-			}
-		}
-		return false;
-	}
+    //	public AuthDAO() {}
+    String es = null;
 
-	
-	//직원 로그인 2
-	public boolean EmployeeLogin(AuthDTO dto) {
-		try {
-			try {
-				pstmt = conn.prepareStatement("select * from employee_tbl where id=?");
-				pstmt.setString(1,dto.getId());
-				rs = pstmt.executeQuery();
-				String tid = null;
-				String tpw = null;
-				while(rs.next()) {
-					tid = rs.getString("id");
-					tpw = rs.getString("pw");
-				}
-				
-				
-				if(tid!=null && tpw!=null) {
-					if(tid.equals(dto.getId()) && tpw.equals(dto.getPw())) {
-						return true;
-					}
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}finally {
-				try {
-					rs.close();
-					pstmt.close();
-					
-				} catch (Exception e2) {
-					// TODO: handle exception
-				}
-			}
-			return false;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}finally {
-			
-		}		
-		
-		
-		return false;
-	}
+    public boolean MemberInsert(MemberDTO dto) {
+        try {
+
+
+            pstmt = conn.prepareStatement("insert into member_tbl values(?,?,?,?,?,?,?)");
+
+            rs = pstmt.executeQuery();
+            String tid = null;
+            while (rs.next()) {
+                if (rs.getBoolean("gender") == true) {
+                    es = "남성";
+                } else if (rs.getBoolean("gender") == false) {
+                    es = "여성";
+                }
+
+                pstmt.setString(1, dto.getNickName());
+                pstmt.setString(2, dto.getId());
+                pstmt.setString(3, dto.getPw());
+                pstmt.setString(4, dto.getName());
+                pstmt.setString(5, dto.getEmail());
+                pstmt.setString(6, dto.getAddr());
+                pstmt.setString(7, es);
+            }
+            int result = pstmt.executeUpdate();
+            if (result != 0) {
+                return true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                pstmt.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        return false;
+    }
+
+
+    //회원 로그인 1
+    public boolean MemberLogin(AuthDTO dto) {
+        try {
+            pstmt = conn.prepareStatement("select id,pw from member_tbl where id=?");
+            pstmt.setString(1, dto.getId());
+            rs = pstmt.executeQuery();
+            String tid = null;
+            String tpw = null;
+            while (rs.next()) {
+                tid = rs.getString("id");
+                tpw = rs.getString("pw");
+            }
+
+            if (tid != null && tpw != null) {
+                if (tid.equals(dto.getId()) && tpw.equals(dto.getPw())) {
+                    return true;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                rs.close();
+                pstmt.close();
+
+            } catch (Exception e2) {
+                // TODO: handle exception
+            }
+        }
+        return false;
+    }
+
+
+    //직원 로그인 2
+    public boolean EmployeeLogin(AuthDTO dto) {
+        try {
+            try {
+                pstmt = conn.prepareStatement("select * from employee_tbl where id=?");
+                pstmt.setString(1, dto.getId());
+                rs = pstmt.executeQuery();
+                String tid = null;
+                String tpw = null;
+                while (rs.next()) {
+                    tid = rs.getString("id");
+                    tpw = rs.getString("pw");
+                }
+
+
+                if (tid != null && tpw != null) {
+                    if (tid.equals(dto.getId()) && tpw.equals(dto.getPw())) {
+                        return true;
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    rs.close();
+                    pstmt.close();
+
+                } catch (Exception e2) {
+                    // TODO: handle exception
+                }
+            }
+            return false;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+
+        }
+
+
+        return false;
+    }
 }
--- src/com/cosview/dto/MemberDTO.java
+++ src/com/cosview/dto/MemberDTO.java
@@ -7,60 +7,85 @@ public class MemberDTO {
 	private String nickName;
 	private String addr;
 	private String email;
+	private boolean gender;
 
-	public MemberDTO(String id, String pw, String name, String nickName, String addr, String email) {
-		super();
+
+	public MemberDTO(String id, String pw, String name, String nickName, String addr, String email, boolean gender) {
 		this.id = id;
 		this.pw = pw;
 		this.name = name;
 		this.nickName = nickName;
 		this.addr = addr;
 		this.email = email;
+		this.gender = gender;
 	}
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getPw() {
 		return pw;
 	}
+
 	public void setPw(String pw) {
 		this.pw = pw;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getNickName() {
 		return nickName;
 	}
+
 	public void setNickName(String nickName) {
 		this.nickName = nickName;
 	}
+
 	public String getAddr() {
 		return addr;
 	}
+
 	public void setAddr(String addr) {
 		this.addr = addr;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
+	public boolean getGender() {
+		return gender;
+	}
+
+	public void setGender(boolean gender) {
+		this.gender = gender;
+	}
+
 	@Override
 	public String toString() {
-		return "MemberDTO [id=" + id + ", pw=" + pw + ", name=" + name + ", nickName=" + nickName + ", addr=" + addr
-				+ ", email=" + email + "]";
+		return "MemberDTO{" +
+				"id='" + id + '\'' +
+				", pw='" + pw + '\'' +
+				", name='" + name + '\'' +
+				", nickName='" + nickName + '\'' +
+				", addr='" + addr + '\'' +
+				", email='" + email + '\'' +
+				", gender='" + gender + '\'' +
+				'}';
 	}
-	
-	
-	
-	
 }
